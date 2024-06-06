17301fac8407e493efeae8bb2943e070554cdc15
liumeng <liumeng1105@outlook.com>
2024-06-06 17:26:04 +0800

    <feat>(app):example

--- example/app/main.cpp
+++ example/app/main.cpp
@@ -1,1 +1,11 @@
-int main() {}
+#include "app/App.h"
+
+int main()
+{
+    lm::app::AutoDump();
+    lm::app::ChangeWorkPath();
+    lm::app::SetConsoleCharsetUTF8();
+    lm::app::SingleApp();
+
+    return 0;
+}
