aa33013b063417a2f7d59309ed765e9dda71c00b
liumeng <liumeng1105@outlook.com>
2024-06-06 14:19:44 +0800

    <feat>(test)

---
+++ test/test_app.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+TEST(AdditionTest, HandlesMixedInput1)
+{
+    EXPECT_EQ(1, 1);
+}
+TEST(AdditionTest, HandlesMixedInput2)
+{
+    EXPECT_EQ(1, 1);
+}
+TEST(AdditionTest, HandlesMixedInput3)
+{
+    EXPECT_EQ(1, 1);
+}
+TEST(AdditionTest, HandlesMixedInput4)
+{
+    EXPECT_EQ(1, 1);
+}
+TEST(AdditionTest, HandlesMixedInput5)
+{
+    EXPECT_EQ(1, 2);
+}
