150406dbf7799f2cb1e9e4bdcbfe0079be96f5e1
liumeng <liumeng1105@outlook.com>
2024-06-14 11:03:58 +0800

    <feat>(qmain):add app lib

--- src/qmain/main.cpp
+++ src/qmain/main.cpp
@@ -1,7 +1,11 @@
-#include "top/config/config.h"
-#include "top/config/version.h"
 #include <qapplication.h>
 #include <qwidget.h>
+#include <qtextcodec.h>
+
+#include "top/config/config.h"
+#include "top/config/version.h"
+
+#include "App.h"
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +13,18 @@ int main(int argc, char* argv[])
 
     app.setApplicationName(TOP_NAME);
     app.setApplicationVersion(TOP_VERSION);
+
+    lm::app::AutoDump();
+
+    if (lm::app::SingleApp() != 0) {
+        return 1;
+    }
+
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+    if (!lm::app::ChangeWorkPath()) {
+        return 2;
+    }
 
     QWidget w;
     w.show();
