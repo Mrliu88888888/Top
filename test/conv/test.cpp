3a421d525f6d4682a55e462cb836fa733f53d2c8
liumeng <liumeng1105@outlook.com>
2024-06-18 17:24:53 +0800

    <feat>(test): add conv test

--- test/conv/test.cpp
+++ test/conv/test.cpp
@@ -5,6 +5,12 @@ TEST(Conv, U82GBK)
 {
     const auto source = std::string(u8"hello世界！.");
     const auto gbk    = lm::coding::UTF8ToGBK(source);
-    const auto utf8   = lm::coding::GBKToUTF8(gbk);
-    ASSERT_EQ(source, utf8);
+    ASSERT_FALSE(gbk.empty());
+}
+
+TEST(Conv, GBK2U8)
+{
+    const auto source = lm::coding::UTF8ToGBK(u8"hello世界！.");
+    const auto utf8   = lm::coding::GBKToUTF8(source);
+    ASSERT_FALSE(utf8.empty());
 }
