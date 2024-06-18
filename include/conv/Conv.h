079f2b656270320a7fcc9ff880900e9982e3ddac
LiuMeng <liumeng1105@outlook.com>
2024-06-19 00:28:13 +0800

    <note>(conv): 注释

--- include/conv/Conv.h
+++ include/conv/Conv.h
@@ -19,19 +19,38 @@
 
 namespace lm {
 namespace coding {
+/// @brief 字符串编码格式转换
 class TOP_CONV_API Convert
 {
 public:
+    /// @brief 构造
+    /// @param fromcode 当前编码
+    /// @param tocode 目标编码
     explicit Convert(const char* fromcode, const char* tocode);
+    /// @brief 析构
     ~Convert();
 
+    /// @brief 编码转换是否可用
+    /// @return true: 可用
+    ///        false: 不可用
     inline bool is_open() const { return ((void*)-1 != conv_); }
+
+    /// @brief 转换
+    /// @param str 字符串
+    /// @return 空: 转换失败
+    ///             目标编码的字符串
     std::string conv(const std::string& str);
 
 private:
     void* conv_;
 };
 
+/// @brief 字符串编码格式转换
+/// @param input 字符串
+/// @param fromcode 当前编码
+/// @param tocode 目标编码
+/// @return 空: 转换失败
+///             目标编码的字符串
 inline std::string ConvertEncoding(const std::string& input, const char* fromcode,
                                    const char* tocode)
 {
