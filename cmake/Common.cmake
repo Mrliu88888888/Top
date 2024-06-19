1df5a81cfc7cca176732e378f28b79bd9ddea450
liumeng <liumeng1105@outlook.com>
2024-06-19 15:44:52 +0800

    <format>(cmake)

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,5 +1,13 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
+if (UNIX)
+	set(CMAKE_CXX_FLAGS "-Wl,-rpath=./")
+	set(CMAKE_C_FLAGS "-Wl,-rpath=./")
+elseif (MSVC)
+	add_compile_options("/utf-8")
+	add_compile_definitions(UNICODE _UNICODE)
+endif ()
+
 set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/${PROJECT_NAME})
 set(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/${PROJECT_NAME})
 
@@ -9,14 +17,6 @@ if (CMAKE_CL_64)
   set(TOP_PLATFORM x64)
 else ()
   set(TOP_PLATFORM x86)
-endif ()
-
-if (UNIX)
-	set(CMAKE_CXX_FLAGS "-Wl,-rpath=./")
-	set(CMAKE_C_FLAGS "-Wl,-rpath=./")
-elseif (MSVC)
-	add_compile_options("/utf-8")
-	add_compile_definitions(UNICODE _UNICODE)
 endif ()
 
 configure_file (
