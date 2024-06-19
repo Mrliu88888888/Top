015748ade76f54880685ae0058b76b56f5cc55ae
liumeng <liumeng1105@outlook.com>
2024-06-19 09:19:10 +0800

    <feat>(app): add test SingleApp

--- test/app/test.cpp
+++ test/app/test.cpp
@@ -5,3 +5,13 @@ TEST(App, ChangeWorkPath)
 {
     ASSERT_TRUE(lm::app::ChangeWorkPath());
 }
+
+TEST(App, SingleApp)
+{
+    ASSERT_EQ(lm::app::SingleApp(), 0);
+}
+
+TEST(App, SingleApp2)
+{
+    ASSERT_EQ(lm::app::SingleApp(), 1);
+}
