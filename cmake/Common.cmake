b69b6e0749b27a9a77132979decb787596e0eaa7
liumeng <liumeng1105@outlook.com>
2024-06-20 09:46:51 +0800

    <format>(cmake): cmake风格修改：空格( 改为 (

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,38 +1,38 @@
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-if (UNIX)
+if(UNIX)
 	set(CMAKE_CXX_FLAGS "-Wl,-rpath=./")
 	set(CMAKE_C_FLAGS "-Wl,-rpath=./")
-elseif (MSVC)
+elseif(MSVC)
 	add_compile_options("/utf-8")
 	add_compile_definitions(UNICODE _UNICODE)
-endif ()
+endif()
 
 set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/${PROJECT_NAME})
 set(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/${PROJECT_NAME})
 
 set(TOP_THIRDPARTY "${CMAKE_CURRENT_SOURCE_DIR}/3rd")
 set(TOP_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/include")
-if (CMAKE_CL_64)
+if(CMAKE_CL_64)
   set(TOP_PLATFORM x64)
-else ()
+else()
   set(TOP_PLATFORM x86)
-endif ()
-if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+endif()
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     set(TOP_DEBUG TRUE)
-else ()
+else()
     set(TOP_DEBUG FALSE)
-endif ()
+endif()
 
-configure_file (
+configure_file(
 	"${CMAKE_CURRENT_SOURCE_DIR}/cmake/include/top/config/version.h.in"
 	"${CMAKE_BINARY_DIR}/include/top/config/version.h"
 )
-configure_file (
+configure_file(
 	"${CMAKE_CURRENT_SOURCE_DIR}/cmake/include/top/config/config.h.in"
 	"${CMAKE_BINARY_DIR}/include/top/config/config.h"
 )
-configure_file (
+configure_file(
 	"${CMAKE_CURRENT_SOURCE_DIR}/cmake/include/top/config/path.h.in"
 	"${CMAKE_BINARY_DIR}/include/top/config/path.h"
 )
