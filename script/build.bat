c1d3e543ae7004ad9f68cef14f0b194a5843b3d7
liumeng <liumeng1105@outlook.com>
2024-06-20 16:11:21 +0800

    <fix>(script): call vc runtime

--- script/build.bat
+++ script/build.bat
@@ -3,10 +3,10 @@
 chcp 65001
 
 set BUILD_TYPE=x64-relwithdebinfo
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
 cmake --preset %BUILD_TYPE% ..
-pushd ../out/build/%BUILD_TYPE%
+cd ../out/build/%BUILD_TYPE%
 cmake --build .
 ctest
 cpack
-popd
