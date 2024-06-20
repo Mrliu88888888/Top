f2a349be1967eb2a7234324ad8cbd9f5d2dd4bbb
liumeng <liumeng1105@outlook.com>
2024-06-20 15:16:22 +0800

    <feat>(app): pack app

--- example/app/main.cpp
+++ example/app/main.cpp
@@ -1,11 +1,34 @@
+#include <iostream>
+
+
 #include "App.h"
+#include "Conv.h"
+#include "Log.h"
+
+using std::cout;
+using std::endl;
 
 int main()
 {
     lm::app::AutoDump();
-    lm::app::ChangeWorkPath();
+
     lm::app::SetConsoleCharsetUTF8();
-    lm::app::SingleApp();
+
+    if (!lm::app::ChangeWorkPath()) {
+        cout << "ChangeWorkPath failed..." << endl;
+        return 1;
+    }
+
+    lm::log::Init("log/app.log", lm::log::LOG_LEVEL_DEBUG, 1024 * 1024 * 10, 3);
+
+    {
+        const auto res = lm::app::SingleApp();
+        if (res != 0) {
+            cout << "SingleApp: " << res << endl;
+            LOG_ERROR("SingleApp: {}", res);
+            return 2;
+        }
+    }
 
     return 0;
 }
