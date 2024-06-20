833ca4de1f812766828e200a28a021078a1d3c39
liumeng <liumeng1105@outlook.com>
2024-06-20 16:00:25 +0800

    <feat>(script): build

--- script/build.bat
+++ script/build.bat
@@ -1,6 +1,12 @@
 @echo off
 
-cmake --preset x64-relwithdebinfo ..
+chcp 65001
+
+set BUILD_TYPE=x64-relwithdebinfo
+
+cmake --preset %BUILD_TYPE% ..
+pushd ../out/build/%BUILD_TYPE%
 cmake --build .
 ctest
 cpack
+popd
