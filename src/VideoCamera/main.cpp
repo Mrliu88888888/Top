11fb4b107ec295727a99a59e4bf62f5deced3ebc
LiuMeng <liumeng1105@outlook.com>
2024-06-24 23:51:11 +0800

    <feat>(VideoCamera): read mp4

--- src/VideoCamera/main.cpp
+++ src/VideoCamera/main.cpp
@@ -1,9 +1,15 @@
 #include <opencv2/opencv.hpp>
 
+#if 0
+#    define CAPTURE_SOURCE 0
+#else
+#    define CAPTURE_SOURCE R"(D:\LiuMeng\Admin\Study\Pr\Pr.mp4)"
+#endif
+
 int main()
 {
     cv::VideoCapture cap;
-    if (cap.open(0)) {
+    if (cap.open(CAPTURE_SOURCE)) {
         cv::Mat frame;
         for (;;) {
             cap >> frame;
@@ -11,7 +17,7 @@ int main()
                 continue;
             }
             cv::imshow("v", frame);
-            if (cv::waitKey() == 'q') {
+            if (cv::waitKey(1) == 'q') {
                 break;
             }
         }
