b9b9a90773f0aa187ff08233321bc45104c2b605
liumeng <liumeng1105@outlook.com>
2024-06-27 11:15:58 +0800

    simple...

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,80 +1,34 @@
 #include <iostream>
+using namespace std;
 
-#include <SimpleIni.h>
-
-#include "top/config/config.h"
-#include "top/config/version.h"
-
-#include "App.h"
-
-#include "pcm.h"
-#include "mnew.h"
-
-struct Config
-{
-    explicit Config()
-        : port(0)
-    {}
-
-    std::string ip;
-    int         port;
-
-    void print() const
-    {
-        using std::cout;
-        using std::endl;
-
-        cout << "ip  : " << ip << endl;
-        cout << "port: " << port << endl;
-    }
-
-    static Config Parse(int argc, char* argv[])
-    {
-        Config conf;
-
-        const auto filename = (2 == argc) ? argv[1] : "conf/top.ini";
-        // if (!std::filesystem::exists(filename)) {
-        //     std::cout << filename << " file not exists" << std::endl;
-        //     return conf;
-        // }
-
-        CSimpleIniA ini;
-        if (SI_OK == ini.LoadFile(filename)) {
-            conf.ip   = ini.GetValue("", "ip", "127.0.0.1");
-            conf.port = ini.GetLongValue("", "port", 0);
-        }
-        return conf;
-    }
-};
-
+/*
+ *                        _oo0oo_
+ *                       o8888888o
+ *                       88" . "88
+ *                       (| -_- |)
+ *                       0\  =  /0
+ *                     ___/`---'\___
+ *                   .' \\|     |// '.
+ *                  / \\|||  :  |||// \
+ *                 / _||||| -:- |||||- \
+ *                |   | \\\  - /// |   |
+ *                | \_|  ''\---/''  |_/ |
+ *                \  .-\__  '-'  ___/-. /
+ *              ___'. .'  /--.--\  `. .'___
+ *           ."" '<  `.___\_<|>_/___.' >' "".
+ *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *          \  \ `_.   \_ __\ /__ _/   .-` /  /
+ *      =====`-.____`.___ \_____/___.-`___.-'=====
+ *                        `=---='
+ *
+ *
+ *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *            佛祖保佑       永不宕机     永无BUG
+ */
 int main(int argc, char* argv[])
 {
-#ifdef _WIN32
-    lm::app::AutoDump();
-
-    lm::app::SetConsoleCharsetUTF8();
-#endif   // _WIN32
-
-    if (lm::app::SingleApp() != 0) {
-        return 1;
-    }
-
-    if (!lm::app::ChangeWorkPath()) {
-        return 2;
-    }
-
-    std::cout << "hello " << TOP_NAME << ": " << TOP_VERSION << std::endl;
-
-    const auto conf = Config::Parse(argc, argv);
-    conf.print();
-
-#ifdef ENABLE_PCM
-    pcm::run();
-#endif
-
-#ifdef ENABLE_MNEW
-    mnew::run();
-#endif
+    cout << "hello Top" << endl;
 
     return 0;
 }
--- src/main/mnew.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace mnew {
-void run();
-}   // namespace mnew
-
-#define ENABLE_MNEW
