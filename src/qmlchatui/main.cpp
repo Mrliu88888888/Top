122fec57fabc8759c7955ca672e51f1459790751
liumeng <liumeng1105@outlook.com>
2024-07-25 09:06:13 +0800

    <feat>(qmlchatui): main

--- src/qmlchatui/main.cpp
+++ src/qmlchatui/main.cpp
@@ -5,10 +5,14 @@
 int main(int argc, char* argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
 
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine("qrc:/qml/WeChat.qml");
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
 
     const auto code = app.exec();
     if (code == 100) {
