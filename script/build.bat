4f117af0be773b44f15c136a343645b808ae8ae8
liumeng <liumeng1105@outlook.com>
2024-12-05 14:33:32 +0800

    <build>(script): 去除BUILD_SHARED_LIBS项

--- script/build.bat
+++ script/build.bat
@@ -3,21 +3,16 @@
 chcp 65001
 
 set BUILD_TYPE="%~1"
-set BUILD_SHARED_LIB="%~2"
 
 if %BUILD_TYPE%=="" (
 	set BUILD_TYPE="x64-relwithdebinfo"
 )
-if %BUILD_SHARED_LIB%=="" (
-	set BUILD_SHARED_LIB="ON"
-)
 
 echo BUILD_TYPE: %BUILD_TYPE%
-echo BUILD_SHARED_LIB: %BUILD_SHARED_LIB%
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
-cmake --preset %BUILD_TYPE% .. -DBUILD_SHARED_LIBS=%BUILD_SHARED_LIB%
+cmake --preset %BUILD_TYPE% ..
 cd ../out/build/%BUILD_TYPE%
 cmake --build .
 ctest
