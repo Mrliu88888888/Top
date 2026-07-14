90a5b8c7822ad0ee4833d1580178b5b1bd568e79
liumeng <liumeng1105@outlook.com>
2026-07-14 15:33:30 +0800

    <style>(cpp): 更新C++代码规范和静态检查

--- src/Top/main.cpp
+++ src/Top/main.cpp
@@ -1,13 +1,17 @@
+#include <qcoreapplication.h>
+#include <qdebug.h>
+#include <qdir.h>
+#include <qglobal.h>
 #include <qguiapplication.h>
+#include <qicon.h>
+#include <qnamespace.h>
+#include <qprocess.h>
 #include <qqmlapplicationengine.h>
 #include <qqmlcontext.h>
-#include <qdir.h>
-#include <qtextcodec.h>
-#include <qprocess.h>
 #include <qresource.h>
-#include <qicon.h>
 #include <qsharedmemory.h>
-#include <qdebug.h>
+#include <qtextcodec.h>
+
 #include "Top/config/config.h"
 #include "Top/config/version.h"
 
@@ -36,50 +40,55 @@
  *
  *            佛祖保佑       永不宕机     永无BUG
  */
-int main(int argc, char* argv[])
-{
-    qputenv("QT_OPENGL", "angle");
+int main(int argc, char* argv[]) {
+  qputenv("QT_OPENGL", "angle");
 
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+  QGuiApplication app(argc, argv);
 
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+  QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 
-    app.setOrganizationName("白日做Meng技术无限公司");
-    app.setApplicationName(TOP_NAME);
-    app.setApplicationVersion(TOP_VERSION);
-    app.setWindowIcon(QIcon(":/res/Top.png"));
+  QGuiApplication::setOrganizationName("白日做Meng技术无限公司");
+  QGuiApplication::setApplicationName(TOP_NAME);
+  QGuiApplication::setApplicationVersion(TOP_VERSION);
+  QGuiApplication::setWindowIcon(QIcon(":/res/Top.png"));
 
-    QSharedMemory sharedMemory(app.applicationName() + app.applicationVersion());
-    if (!sharedMemory.create(1, QSharedMemory::ReadWrite)) {
-        qDebug() << "SingleApp";
-        return 1;
-    }
+  QSharedMemory sharedMemory(QGuiApplication::applicationName() +
+                             QGuiApplication::applicationVersion());
+  if (!sharedMemory.create(1, QSharedMemory::ReadWrite)) {
+    qDebug() << "SingleApp";
+    return 1;
+  }
 
-    if (!QDir::setCurrent(app.applicationDirPath())) {
-        qDebug() << "WorkingDirectory";
-        return 2;
-    }
+  if (!QDir::setCurrent(QGuiApplication::applicationDirPath())) {
+    qDebug() << "WorkingDirectory";
+    return 2;
+  }
 
-    if (!QResource::registerResource("rcc/qml.rcc")) {
-        qDebug() << "registerResource qml.rcc failed";
-        return 3;
-    }
+  if (!QResource::registerResource("rcc/qml.rcc")) {
+    qDebug() << "registerResource qml.rcc failed";
+    return 3;
+  }
 
-    QQmlApplicationEngine engine;
-    engine.load("qrc:/qml/App.qml");
-    if (engine.rootObjects().isEmpty()) {
-        qDebug() << "QQmlApplicationEngine rootObjects isEmpty";
-        return -1;
-    }
-    const auto code = app.exec();
+  QQmlApplicationEngine engine;
+  engine.load("qrc:/qml/App.qml");
+  if (engine.rootObjects().isEmpty()) {
+    qDebug() << "QQmlApplicationEngine rootObjects isEmpty";
+    return -1;
+  }
+  const auto code = QGuiApplication::exec();
 
-    if (!sharedMemory.detach()) {
-        qDebug() << "detach shared memory";
-    }
-    switch (code) {
-    case 10: QProcess::startDetached(app.applicationFilePath(), app.arguments()); break;
-    }
-    return code;
+  if (!sharedMemory.detach()) {
+    qDebug() << "detach shared memory";
+  }
+  switch (code) {
+    case 10:
+      QProcess::startDetached(QGuiApplication::applicationFilePath(),
+                              QGuiApplication::arguments());
+      break;
+    default:
+      break;
+  }
+  return code;
 }
